99d75f0d643874120e608902d446d9dcf74360b5
Neil Wilson-Harris <251717455+nawh-ops@users.noreply.github.com>
2026-03-07 11:13:45 +0000

    Initial Commit

---
+++ RouteBuddyBeacon/RouteBuddyBeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteBuddyBeaconApp.swift
+//  RouteBuddyBeacon
+//
+//  Created by Neil Wilson-Harris on 07/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct RouteBuddyBeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
