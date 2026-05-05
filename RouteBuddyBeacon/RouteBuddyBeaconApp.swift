1938282301115e501643cac860ddcf162e298c72
Neil Wilson-Harris <251717455+nawh-ops@users.noreply.github.com>
2026-05-05 15:39:04 +0100

    Beacon: lock app to portrait orientation

--- RouteBuddyBeacon/RouteBuddyBeaconApp.swift
+++ RouteBuddyBeacon/RouteBuddyBeaconApp.swift
@@ -6,9 +6,19 @@
 //
 
 import SwiftUI
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
 
 @main
 struct RouteBuddyBeaconApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
